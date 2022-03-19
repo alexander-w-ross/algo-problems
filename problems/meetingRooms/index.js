f3b04b7b0e0200c491e5ebec0d912b2985306cad
Alex Ross <alexander.w.rosss@gmail.com>
2022-03-18 20:32:31 -0400

    add meeting rooms code (locked question)

---
+++ problems/meetingRooms/index.js
@@ -0,0 +1,26 @@
+// Given an array of meeting time intervals consisting of start and end times [s1, e1], [s2, e2], ... , determine if a person could attend all meetings.
+
+// For example,
+// Given [ [0, 30], [5, 10], [15, 20] ],
+// return false.
+// Input:
+//  [[7,10],[2,4], [15,18]]
+
+// Output:
+//  true
+
+// !locked question
+
+function meetingRooms(meetingTimes) {
+  // sort by start time
+  meetingTimes.sort((a, b) => a[0] - b[0]);
+  for (let i = 0; i < meetingTimes.length - 1; i++) {
+    let endTime = meetingTimes[i][1];
+    if (endTime > meetingTimes[i + 1][0]) console.log(false);
+  }
+  console.log(true);
+}
+meetingRooms([
+  [7, 10],
+  [2, 4],
+]);
