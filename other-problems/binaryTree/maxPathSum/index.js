f675353303ea9d9dfbfe0888847c90a895bbe91e
Alex Ross <alexander.w.rosss@gmail.com>
2022-03-31 22:08:31 -0400

    add maxSumPath

---
+++ other-problems/binaryTree/maxPathSum/index.js
@@ -0,0 +1,5 @@
+const maxPathSum = (root) => {
+  if (!root) return -Infinity;
+  if (!root.left && !root.right) return root.val;
+  return root.val + Math.max(maxPathSum(root.left), maxPathSum(root.right));
+};
