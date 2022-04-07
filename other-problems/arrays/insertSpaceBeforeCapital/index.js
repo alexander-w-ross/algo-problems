a1db4d1116580ce28c78f383b50570114d2500d2
Alex Ross <alexander.w.rosss@gmail.com>
2022-04-07 08:49:00 -0400

    remove consolelog

--- other-problems/arrays/insertSpaceBeforeCapital/index.js
+++ other-problems/arrays/insertSpaceBeforeCapital/index.js
@@ -1,7 +1,6 @@
 const insertSpaceBeforeCapital = (str) => {
   let tmp = str.split("");
   for (let i = 1; i < tmp.length; i++) {
-    console.log(str[i]);
     if (tmp[i] === tmp[i].toUpperCase()) {
       tmp[i - 1] += " ";
     }
